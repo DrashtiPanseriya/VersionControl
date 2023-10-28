3d57accc54d6c8032e5b8c032059f1db62708ab7
Drashti <panseriyadrashti@gmail.com>
2023-10-28 14:08:22 +0530

    second commit

--- first/models.py
+++ first/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=15, unique=True, primary_key=True)
+    first_name = models.CharField(max_length=20, blank=False)
+    last_name = models.CharField(max_length=20)
+    contact_number = models.CharField(max_length=10)
+    password = models.CharField(max_length=128, default="")
+    gender = models.CharField(max_length=1, choices= [
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other'),
+    ])
+    age = models.PositiveIntegerField()   
+
